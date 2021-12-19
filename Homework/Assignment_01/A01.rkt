646846dead481bc76ec51f940c65148b31bfa2f5
Van <51791613+pavaniravella@users.noreply.github.com>
2021-12-19 17:27:09 -0500

    practice
    redid assignments 1 and 2 for practice.

--- Homework/Assignment_01/A01.rkt
+++ Homework/Assignment_01/A01.rkt
@@ -2,45 +2,63 @@
 
 (provide interval-contains? interval-intersects? interval-union my-first my-second my-third make-vec-from-points dot-product vector-magnitude distance)
 
+
 (define interval-contains?
-  (lambda (a b)
-    (nyi)))
+  (lambda (interval number)
+    (and (<= number (cadr interval)) (>= number (car interval)))))
 
-(define interval-intersects?
-  (lambda (a b)
-    (nyi)))
 
-(define interval-union
-  (lambda (a b)
-    (nyi)))
+
+(define interval-intersects? 
+  (lambda (i1 i2)
+    (or (or (interval-contains? i1 (car i2)) (interval-contains? i1 (cadr i2))) 
+        (or (interval-contains? i2 (car i1)) (interval-contains? i2 (cadr i1))))))
+
+
+(define interval-union 
+  (lambda (i1 i2)
+    (if (interval-intersects? i1 i2)
+        (list (list (min (car i1) (car i2)) (max (cadr i1) (cadr i2))))
+        (list i1 i2))))
+
 
 (define my-first
   (lambda (a)
-    (nyi)))
+    (car a)))
 
 (define my-second
   (lambda (a)
-    (nyi)))
+    (cadr a)))
 
 (define my-third
   (lambda (a)
-    (nyi)))
+    (caddr a)))
+
 
 (define make-vec-from-points
-  (lambda (a b)
-    (nyi)))
+  (lambda (l1 l2)
+    (list (- (my-first l2) (my-first l1)) (- (my-second l2) (my-second l1)) (- (my-third l2) (my-third l1)))
+    ))	
+
 
 (define dot-product
-  (lambda (a b)
-    (nyi)))
+  (lambda (l1 l2)
+    (+ (* (my-first l2) (my-first l1)) (* (my-second l2) (my-second l1)) (* (my-third l2) (my-third l1)))))
+
 
 (define vector-magnitude
-  (lambda (a)
-    (nyi)))
+  (lambda (ls)
+    (sqrt (+ (square (car ls)) (square (cadr ls)) (square (my-third ls))))))
 
+
+(define square
+  (lambda (a)
+    (* a a)
+    )
+  )
 (define distance
-  (lambda (a b)
-    (nyi)))
+  (lambda (l1 l2)
+    (vector-magnitude(make-vec-from-points l1 l2))))
 
 ;;--------  Used by the testing mechanism   ------------------
 
--- Homework/Assignment_02/A02.rkt
+++ Homework/Assignment_02/A02.rkt
@@ -2,38 +2,80 @@
 
 (provide choose sum-of-squares range my-set? union cross-product parallel? collinear?)
 
+(define (fact n)
+  (if (<= n 1)
+      1
+      (* n (fact (sub1 n)))
+      )
+  )
 (define choose
   (lambda (a b)
-    (nyi)))
+    (/ (fact a) (* (fact b)(fact (- a b)) ))))
 
 (define sum-of-squares
-  (lambda (a)
-    (nyi)))
+  (lambda (ls)
+    (cond
+      [(null? ls) 0]
+      [else
+       (+ (* (first ls) (first ls)) (sum-of-squares (rest ls)))
+       ]
+      )))
 
 (define range
-  (lambda (a b)
-    (nyi)))
+  (lambda (n m)
+    (cond
+      [(>= n m) '()]
+      [(cons n (range (add1 n) m))]
+      )))
 
-(define my-set?
-  (lambda (a)
-    (nyi)))
+(define (my-set? ls)
+  (cond
+    [(null? ls) #t]
+    [(cons? ls)
+     ;; check if that first element is unique 
+     ;;   if so, produce #t iff the rest of the elements are each unique
+     ;;   if not, produce #f 
+     (and (unique-element? (first ls) (rest ls)) (my-set? (rest ls)))])) ;;check if the rest of the elements are unique amongst themselves 
 
+;;checks if the given element is not contained in the ls 
+;;
+(define (unique-element? element ls)
+  (cond
+    [(null? ls) #t]
+    [(cons? ls)
+     (if (equal? element (first ls))
+         #f
+         (unique-element? element (rest ls))
+         )]))
+
+;;along for the ride template 
 (define union
   (lambda (a b)
-    (nyi)))
+    (cond
+      [(null? a) b]
+      [(cons? a)
+       (if (member (first a) b)
+           (union (rest a) b)
+           (cons (first a) (union (rest a) b))
+           
+           )
+       ])))
 
 (define cross-product
-  (lambda (a b)
-    (nyi)))
+  (lambda (v1 v2)
+    (list (- (* (second v1) (third v2)) (* (third v1) (second v2)))
+          (- (* (third v1) (first v2)) (* (first v1) (third v2)))
+          (- (* (first v1) (second v2)) (* (second v1) (first v2))))
+    ))
 
 (define parallel?
-  (lambda (a b)
-    (nyi)))
+  (lambda (v1 v2)
+    (and (and (zero? (first (cross-product v1 v2))) (zero? (second (cross-product v1 v2)))) (zero? (third (cross-product v1 v2))))))
 
 (define collinear?
-  (lambda (a b c)
-    (nyi)))
-
+  (lambda (p1 p2 p3)
+    (parallel? (list (- (first p1) (first p2)) (- (second p1) (second p2)) (- (third p1) (third p2)))
+               (list (- (first p3) (first p2)) (- (second p3) (second p2)) (- (third p3) (third p2))))))
 ;;--------  Used by the testing mechanism   ------------------
 
 (define-syntax nyi
