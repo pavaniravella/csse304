686561efc4ac45642749ef8976290c855e02cb78
Van <51791613+pavaniravella@users.noreply.github.com>
2021-12-19 18:10:24 -0500

    Update A03.rkt
    practiced

--- Homework/Assignment_03/A03.rkt
+++ Homework/Assignment_03/A03.rkt
@@ -4,19 +4,61 @@
 
 (define intersection
   (lambda (a b)
-    (nyi)))
+    (cond
+      [(null? a) '()]
+      [(cons? a)
+       (if (member (first a) b)
+           (cons (first a)(intersection (rest a) b))
+           (intersection (rest a) b)
+           )
+       ]
+      )))
 
 (define subset?
   (lambda (a b)
-    (nyi)))
+    (cond
+      [(null? a) #t]
+      [else
+       (and (member (first a) b) (subset? (rest a) b))
+       ]
+      )))
+
 
 (define relation?
-  (lambda (a)
-    (nyi)))
+  (lambda (x)
+    (and (set? x) (andmap list? x) (andmap is-size-2? x))))
+
+;;helper method to check if each element is the sie two 
+(define (is-size-2? ls)
+  (= (length ls) 2))
+
+;;helper functions
+(define (all-unique-elements? ls)
+  (cond
+    [(null? ls) #t]
+    [ (cons? ls)
+      (and (unique-element? (car ls) (cdr ls)) (all-unique-elements?(cdr ls)))]))
+
+(define (unique-element? element ls)
+  (cond
+    [(null? ls) #t]
+    [(cons? ls)
+     (if (equal? element (first ls))
+         #f
+         (unique-element? element (cdr ls)))]))
+
+(define (set? x)
+  (and (list? x) (all-unique-elements? x)))
+
 
 (define domain
   (lambda (a)
-    (nyi)))
+    (cond
+      [(null? a) '()]
+      [else
+       (cons (first (first a)) (domain (rest a)))
+       ]
+      )))
 
 (define reflexive?
   (lambda (a)
@@ -24,19 +66,34 @@
 
 (define multi-set?
   (lambda (a)
-    (nyi)))
+    (cond
+      [(null? a) #t]
+      
+      [else
+       (and (list? a)(list? (first a)) (multi-set? (rest a)))
+       ]
+      )))
 
 (define ms-size
   (lambda (a)
-    (nyi)))
+    (apply + (map second a))))
 
 (define last
   (lambda (a)
-    (nyi)))
+    (cond
+      [(null? (rest a)) (first a)]
+      [else
+       (last (rest a))
+       ]
+      )))
 
 (define all-but-last
   (lambda (a)
-    (nyi)))
+    (cond
+      [(null? (rest a)) '()]
+      [(cons (first a) (all-but-last (rest a)))]
+
+      )))
 
 ;;--------  Used by the testing mechanism   ------------------
 
