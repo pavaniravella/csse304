735fb5e1945d277f12237359aacb6b850309ea3f
Van <51791613+pavaniravella@users.noreply.github.com>
2021-12-21 18:33:27 -0500

    Update A05.rkt

--- Homework/Assignment_05/A05.rkt
+++ Homework/Assignment_05/A05.rkt
@@ -2,25 +2,68 @@
 
 (provide minimize-interval-list exists? product replace remove-last)
 
+;;need to figure out how to make this a better and more simpler way
 (define minimize-interval-list
-  (lambda (a)
-    (nyi)))
+  (lambda (lst)
+    (if (eq? (length lst) 1)
+        lst
+        (let ([sortedlst (sort lst (lambda (pr1 pr2) (< (car pr1) (car pr2))) )])
+          (if (>= (cadar sortedlst) (caadr sortedlst))
+              (minimize-interval-list [cons (list [caar sortedlst] (max [cadadr sortedlst] [cadar sortedlst])) (cddr sortedlst)])
+              [cons (car sortedlst) (minimize-interval-list (cdr sortedlst))])))))
 
 (define exists?
-  (lambda (a b)
-    (nyi)))
+  (lambda (pred? ls)
+    (cond
+      [(null? ls) #f ]
+      [else
+       (or (pred? (first ls)) (exists? pred? (rest ls)))
+       ]
+
+      )))
 
 (define product
-  (lambda (a b)
-    (nyi)))
+  (lambda (ls1 ls2)
+    (cond
+      [(null? ls1) '()]
+      [(null? ls2) '()]
+      [else
+       (append (product-helper (first ls1) ls2) (product (rest ls1) ls2))
+       ]
+      )))
+
+(define (product-helper x ls)
+  (cond
+    [(null? ls) '()]
+    [(cons (list  x (first ls)) (product-helper x (rest ls)))]
+
+    )
+  )
 
 (define replace
-  (lambda (a b c)
-    (nyi)))
+  (lambda (old new ls)
+    (cond
+      [(null? ls) '() ]
+      [(equal? (car ls) old) (cons new (replace old new (rest ls)))]
+      [else
+       ( cons (first ls) (replace old new (rest ls)))
+       ]
+      )))
 
+(define (remove-first element ls)
+  (cond
+    [(null? ls) '()]
+    [(equal? element (first ls))
+     (append '() (rest ls))
+     ]
+    [(cons (first ls) (remove-first element (rest ls)))]
+
+    )
+
+  )
 (define remove-last
-  (lambda (a b)
-    (nyi)))
+  (lambda (element ls)
+    (reverse (remove-first element (reverse ls)))))
 
 ;;--------  Used by the testing mechanism   ------------------
 
